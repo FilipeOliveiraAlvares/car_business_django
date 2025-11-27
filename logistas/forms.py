bb0c8845587d277b9210e47e23af84ef15d45584
Filipe Alvares <alvares.filipeoliveira@gmail.com>
2025-11-27 09:55:41 -0300

    Corrigir validacao de URLs no formulario de loja - normalizar instagram e facebook

--- logistas/forms.py
+++ logistas/forms.py
@@ -21,8 +21,27 @@ class LojaForm(forms.ModelForm):
             'endereco': forms.TextInput(attrs={'placeholder': 'Rua, número, bairro...'}),
             'telefone': forms.TextInput(attrs={'placeholder': '(99) 99999-9999'}),
             'whatsapp': forms.TextInput(attrs={'placeholder': '(99) 99999-9999 ou 5599999999999'}),
-            'instagram': forms.TextInput(attrs={'placeholder': '@sualoja'}),
-            'facebook': forms.TextInput(attrs={'placeholder': 'facebook.com/sualoja'}),
+            'instagram': forms.URLInput(attrs={'placeholder': 'https://instagram.com/sualoja'}),
+            'facebook': forms.URLInput(attrs={'placeholder': 'https://facebook.com/sualoja'}),
             'site': forms.URLInput(attrs={'placeholder': 'https://www.sualoja.com'}),
             'maps_url': forms.URLInput(attrs={'placeholder': 'Cole aqui o link do Google Maps'}),
         }
+    
+    def clean_instagram(self):
+        """Valida e normaliza URL do Instagram"""
+        instagram = self.cleaned_data.get('instagram', '').strip()
+        if instagram and not instagram.startswith(('http://', 'https://')):
+            # Se não começar com http, adiciona https://
+            if instagram.startswith('@'):
+                instagram = f'https://instagram.com/{instagram[1:]}'
+            elif instagram:
+                instagram = f'https://{instagram}'
+        return instagram if instagram else None
+    
+    def clean_facebook(self):
+        """Valida e normaliza URL do Facebook"""
+        facebook = self.cleaned_data.get('facebook', '').strip()
+        if facebook and not facebook.startswith(('http://', 'https://')):
+            # Se não começar com http, adiciona https://
+            facebook = f'https://{facebook}'
+        return facebook if facebook else None
